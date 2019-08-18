f8e08d4a559741618409dae9b9f2e4666a3a21c3
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-17 21:35:17 -0300

    Danldelion First COmmit

--- Test VR Hackaton SNX/Assets/ScalingObject.cs
+++ Test VR Hackaton SNX/Assets/ScalingObject.cs
@@ -7,6 +7,13 @@ public class ScalingObject : MonoBehaviour
     public AnimationCurve curve;
     public float amounToMultiply = 10;
     public float debugvariable;
+
+    public Vector3 ScaleUp;
+    public Vector3 scaleDefault;
+
+    public bool buttonScale;
+
+    //public bool 
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +23,18 @@ public class ScalingObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateVariable();
+       // UpdateVariable();
+            buttonScale = Input.GetButton("Jump");
+
+
+
+        if(Input.GetButtonDown("Jump")){
+            ScaleScenaryUp();
+        }
+
+        if(Input.GetButtonUp("Jump")){
+            ScaleNormal();
+        }
     }
 
     public void UpdateVariable(){
@@ -28,6 +46,11 @@ public class ScalingObject : MonoBehaviour
 
 
     public void ScaleScenaryUp() {
-        
+        Debug.Log("SetToMinScale");
+        iTween.ScaleTo(gameObject,ScaleUp,3);
+    }
+
+    public void ScaleNormal(){
+        iTween.ScaleTo(gameObject, scaleDefault ,3);
     }
 }
